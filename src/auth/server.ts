b740f6457ba56050d23e27060bf6049b492b75fb
Matt <Geczy@users.noreply.github.com>
2025-10-04 15:32:14 -0500

    Revert "Refactor authentication logic for improved readability and maintainability"
    This reverts commit b67d453667cad5f9c4ad343666ba05f09a5bc2b5.

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -1,63 +1,51 @@
-import NextAuth, { type User } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { isPathProtected } from "@/app/path";
+import { isPathProtected } from '@/app/path';
+import NextAuth, { User } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const {
-	handlers: { GET, POST },
-	signIn,
-	signOut,
-	auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
 } = NextAuth({
-	providers: [
-		Credentials({
-			async authorize({ email, password }) {
-				console.log("Attempting to authorize user:", {
-					email,
-					password,
-					adminEmail: process.env.ADMIN_EMAIL,
-					adminPassword: process.env.ADMIN_PASSWORD,
-				});
-				if (
-					process.env.ADMIN_EMAIL &&
-					process.env.ADMIN_EMAIL === email &&
-					process.env.ADMIN_PASSWORD &&
-					process.env.ADMIN_PASSWORD === password
-				) {
-					const user: User = { email, name: "Admin User" };
-					return user;
-				} else {
-					return null;
-				}
-			},
-		}),
-	],
-	callbacks: {
-		authorized({ auth, request }) {
-			const { pathname } = request.nextUrl;
+  providers: [
+    Credentials({
+      async authorize({ email, password }) {
+        if (
+          process.env.ADMIN_EMAIL && process.env.ADMIN_EMAIL === email &&
+          process.env.ADMIN_PASSWORD && process.env.ADMIN_PASSWORD === password
+        ) {
+          const user: User = { email, name: 'Admin User' };
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request }) {
+      const { pathname } = request.nextUrl;
 
-			const isUrlProtected = isPathProtected(pathname);
-			const isUserLoggedIn = !!auth?.user;
-			const isRequestAuthorized = !isUrlProtected || isUserLoggedIn;
-			console.log("Attempting to authorize user 2:", {
-				isUrlProtected,
-				isUserLoggedIn,
-				isRequestAuthorized,
-			});
-			return isRequestAuthorized;
-		},
-	},
-	pages: {
-		signIn: "/sign-in",
-	},
+      const isUrlProtected = isPathProtected(pathname);
+      const isUserLoggedIn = !!auth?.user;
+      const isRequestAuthorized = !isUrlProtected || isUserLoggedIn;
+
+      return isRequestAuthorized;
+    },
+  },
+  pages: {
+    signIn: '/sign-in',
+  },
 });
 
 export const runAuthenticatedAdminServerAction = async <T>(
-	callback: () => T,
+  callback: () => T,
 ): Promise<T> => {
-	const session = await auth();
-	if (session?.user) {
-		return callback();
-	} else {
-		throw new Error("Unauthorized server action request");
-	}
+  const session = await auth();
+  if (session?.user) {
+    return callback();
+  } else {
+    throw new Error('Unauthorized server action request');
+  }
 };
